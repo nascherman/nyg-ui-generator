25d312a75524b9ec933fb9bed21a724372bab711
vadim <vadim@jam3.com>
2016-07-20 15:22:20 -0400

    options for generating boilerplate files

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const fs = require('fs');
-const nygModuleGenerator = require('nyg-module-generator');
 const path = require('path');
 const spawn = require('npm-execspawn');
-const tape = require('tape');
+const mkdir = require('mkdir-p');
+const open = require('opn');
+const chalk = require('chalk');
+const nyg = require('nyg');
+const nygModuleGenerator = require('nyg-module-generator');
+
+const promptsInit = [
+  {
+    type: "list",
+    message: "What would you like to do?",
+    name: "action",
+    choices: [
+      {
+        name: "Generate UI boilerplate files",
+        value: "boilerplate",
+        checked: true
+      },
+      {
+        name: "Create UI module",
+        value: "module"
+      },
+      {
+        name: "Publish existing component as module",
+        value: "postinstall"
+      }
+    ]
+  }
+];
 
 const prompts = [
+  {
+    type: "input",
+    name: "component",
+    message: "Name your component:",
+    default: "MyComponent"
+  },
   {
     type: "list",
     message: "Choose UI type:",
@@ -24,17 +56,26 @@ const prompts = [
         value: "bigwheel"
       }
     ]
-  },
+  }
+];
+
+const promptDetails = [
   {
-    type: "input",
-    name: "component",
-    message: "Name your component:",
-    default: "NewComponent"
+    'name': 'location',
+    'message': 'Where would you like to create your component folder?',
+    'default': process.cwd()
   }
 ];
 
 const globs = [
   {base: path.join(__dirname, 'templates/{{type}}'), output: '/'},
+];
+
+const globsSome = [
+  {base: path.join(__dirname, 'templates/{{type}}/'), glob: '*.js', output: '/'},
+  {base: path.join(__dirname, 'templates/{{type}}/'), glob: '*.scss', output: '/'},
+  {base: path.join(__dirname, 'templates/{{type}}/'), glob: '*.less', output: '/'},
+  {base: path.join(__dirname, 'templates/{{type}}/'), glob: '*.hbs', output: '/'}
 ];
 
 const callback = (cwd = globs.output) => {
@@ -43,4 +84,56 @@ const callback = (cwd = globs.output) => {
   spawn(cmd, {cwd});
 };
 
-nygModuleGenerator({prompts, globs, callback});
+const gen = nyg(promptsInit, []);
+gen.on('postprompt', () => {
+  const action = gen.config.get('action');
+  const opts = {prompts, globs, callback};
+  const isModule = (action === 'module');
+  const isPostInstall = (action === 'postinstall');
+
+  if (isModule || isPostInstall) {
+    nygModuleGenerator({prompts, globs, callback, isPostInstall});
+  } else {
+    console.log('your cwd:', process.cwd());
+    var outputDir;
+
+    // copy only UI related files
+    let _prompts = promptDetails.concat(prompts);
+    globsSome.forEach((g) => g.base = path.relative(__dirname, g.base));
+
+    let _gen = nyg(_prompts, globsSome)
+      .on('postprompt', () => {
+        outputDir = path.join(gen.config.get('location'), gen.config.get('component'));
+        _gen.chdir(outputDir);
+
+        console.log('component dir:', outputDir);
+
+        // check if directory exists
+        try {
+          fs.accessSync(outputDir);
+
+          const contents = fs.readdirSync(outputDir);
+          contents.forEach((item, index) => {
+            (item.indexOf('.') === 0) && contents.splice(index, 1);
+          });
+
+          if (contents.length) {
+            console.warn(chalk.bgYellow('WARN'), chalk.magenta(`directory ${outputDir} already exists and is not empty.`));
+            _gen.end();
+          }
+        } catch (e) {
+          mkdir.sync(outputDir);
+        }
+      })
+      .on('postinstall', () => {
+        // clean up
+        let cmd = 'rm -r nyg-cfg.json';
+        spawn(cmd, {cwd: globs.output});
+        spawn(cmd, {cwd: outputDir});
+
+        open(outputDir);
+      })
+      .run();
+  }
+});
+gen.run();
