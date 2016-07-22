09024d24df0145d3a6af5ab11afd8edcfc79f9bc
vadim <vadim@jam3.com>
2016-07-22 00:27:40 -0400

    adding post publish stuff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const spawn = require('npm-execspawn');
+const chalk = require('chalk');
 const nyg = require('nyg');
 const moduleGenerator = require('nyg-module-generator');
 const filesGenerator = require('./lib/filesGenerator');
@@ -40,7 +41,7 @@ const promptName = [
 const promptType = [
   {
     type: "list",
-    message: "Choose UI type:",
+    message: "Select UI type:",
     name: "type",
     choices: [
       {
@@ -61,7 +62,6 @@ const promptType = [
 ];
 
 const prompts = promptName.concat(promptType);
-const promptsPostPublish = promptType;
 
 const globs = [
   {base: path.join(__dirname, 'templates/{{type}}'), output: '/'},
@@ -79,18 +79,55 @@ const callback = (cwd = globs.output) => {
   spawn(cmd, {cwd});
 };
 
+const readConfigs = (gen) => {
+  var configs = {};
+
+  fs.readFile('nyg-cfg.json', 'utf8', (err, data) => {
+    if (err) {
+      console.warn(chalk.bgMagenta('WARN:'), chalk.magenta(`Could not open nyg-cfg.json from ${process.cwd()}.`));
+    } else {
+      configs = JSON.parse(data);
+      if (!configs.type) {
+        console.log(' ');
+        console.warn(chalk.bgMagenta('WARN:'), chalk.magenta(`Cannot get 'type' from ${process.cwd()}/nyg-cfg.json, so you need to select it below.`));
+        console.log(' ');
+      }
+    }
+
+    const type = configs.type;
+    const opts = {
+      prompts: [],
+      globs: globsPostPublish,
+      isPostPublish: true,
+      type
+    };
+
+    if (!type) {
+      gen.prompt(promptType, (data) => {
+        opts.type = data.type;
+        moduleGenerator(opts);
+      })
+    } else {
+      moduleGenerator(opts);
+    }
+  });
+};
+
 const gen = nyg(promptAction, [])
   .on('postprompt', () => {
     const action = gen.config.get('action');
-    const isBoilerplate = (action === 'boilerplate');
-    const isModule = (action === 'module');
-    const isPostPublish = (action === 'postpublish');
-    const opts = {prompts, globs, isPostPublish, promptsPostPublish, globsPostPublish, callback};
 
-    if (isModule || isPostPublish) {
-      moduleGenerator(opts);
-    } else if (isBoilerplate) {
-      filesGenerator(prompts);
+    switch (action) {
+      case 'module':
+        moduleGenerator({prompts, globs, callback});
+        break;
+      case 'boilerplate':
+        filesGenerator(prompts);
+        break;
+      case 'postpublish':
+        const done = gen.async(); // important to prevent config file overwrite before reading
+        readConfigs(gen);
+        break;
     }
   })
   .run();
